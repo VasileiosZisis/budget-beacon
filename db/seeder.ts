f08a977533e08147866fc3dd9c057af0a9abe18b
Vasileios Zisis <vasil.zisis@gmail.com>
2025-06-11 22:24:11 +0300

    made changes to the models

--- db/seeder.ts
+++ db/seeder.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 async function main () {
   const uncategorized = await prisma.category.upsert({
     where: { name: 'Uncategorized' },
-    update: { name: 'Uncategorized' },
+    update: {},
     create: { name: 'Uncategorized' }
   })
 
@@ -17,30 +17,33 @@ async function main () {
 
   await prisma.income.upsert({
     where: { name: 'Job' },
-    update: { price: 1200, categoryId: uncategorized.id },
+    update: { price: 1200, totalPrice: 1200, categoryId: uncategorized.id },
     create: {
       name: 'Job',
       price: 1200,
+      totalPrice: 1200,
       categoryId: uncategorized.id
     }
   })
 
   await prisma.expense.upsert({
     where: { name: 'Rent' },
-    update: { price: 250, categoryId: utilities.id },
+    update: { price: 250, totalPrice: 250, categoryId: utilities.id },
     create: {
       name: 'Rent',
       price: 250,
+      totalPrice: 250,
       categoryId: utilities.id
     }
   })
 
   await prisma.expense.upsert({
     where: { name: 'Market' },
-    update: { price: 400, categoryId: uncategorized.id },
+    update: { price: 400, totalPrice: 400, categoryId: uncategorized.id },
     create: {
       name: 'Market',
       price: 400,
+      totalPrice: 400,
       categoryId: uncategorized.id
     }
   })
